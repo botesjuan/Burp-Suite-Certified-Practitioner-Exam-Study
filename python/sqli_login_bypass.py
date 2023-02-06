c419404b6eba279dc69fa08408f29bf62b122d26
Juan Botes <52822072+botesjuan@users.noreply.github.com>
2023-02-06 20:41:30 +0200

    lab automation

--- python/sqli_login_bypass.py
+++ python/sqli_login_bypass.py
@@ -1,3 +1,4 @@
+import re
 import sys
 import logging
 import urllib3
@@ -19,13 +20,32 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 def main(args):
-    shops = Shop(args.url)  # login url build into the class and this object
+    shop = Shop(args.url)  # login url build into the class and this object
     if args.no_proxy:
-        resp = requests.get(shops.login_url)
+        resp = requests.get(shop.login_url)
     else:
-        resp = requests.get(shops.login_url, proxies=utils.PROXIES, verify=False)
-    if resp.status_code == 200:
-        print(resp.text)
+        resp = requests.get(shop.login_url, proxies=utils.PROXIES, verify=False)
+    if not resp.status_code == 200:
+        log.error("Could not get login page")
+        sys.exit()
+    else:
+        # print(resp.text) # debugging 
+        pattern = re.compile(r'name="csrf" value="(.*?)"')
+        m = pattern.search(resp.text)
+        csrf_token = m[1]
+        data = {
+            "csrf": csrf_token,
+            "username": "",
+            "password": "junkpass",
+        }
+        if args.no_proxy:    #    provide option to set proxy or run script with no proxy
+            resp = request.post(shop.login_url, data=data)
+        else:
+            resp = request.post(
+                shop.login_url, data=data, proxies=utils.PROXIES, verify=False
+            )
+        if resp.status_code == 200:
+            utils.is_solved(shop.base_url)
 
 
 if __name__ == "__main__":
